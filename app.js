6bb83f682c4aebddc78c174bbd2751a1c56be8e5
Sahil-Sukhdeve12 <sahilsukhdeve15@gmail.com>
2024-02-25 17:15:51 +0530

    day-4

--- app.js
+++ app.js
@@ -1,21 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-// const heading=React.createElement("h1",{id:"heading",xyz:"abc"},"Hello world from React!!");
-// console.log(heading);
+const heading=React.createElement("h1",{id:"heading"},"hello");
+console.log(heading);
 
-// root.render(heading);
+const root=ReactDOM.createRoot(document.getElementById("root"));
 
-const parent=React.createElement("div",{id:"parent"},[
-    React.createElement("div",{id:"child"},[
-    React.createElement("h1",{},"I'm an h1 tag"),
-    React.createElement("h2",{},"I'm an h2 tag"),
-]),
-    React.createElement("div",{id:"child"},[
-    React.createElement("h1",{},"I'm an h1 tag"),
-    React.createElement("h2",{},"I'm an h2 tag"),]),
-    ]);
-
-console.log(parent); //object
-const root=ReactDOM.createRoot(document.getElementById("root") );
-root.render(parent);
+root.render(heading);
