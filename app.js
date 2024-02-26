c5521c11f1fb43183badb2a7a086d5f07fc7fff1
Sahil-Sukhdeve12 <sahilsukhdeve15@gmail.com>
2024-02-26 12:25:48 +0530

    jsx 26 feb

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const heading=React.createElement("h1",{id:"heading"},"hello");
-console.log(heading);
+//jsx
+
+const jsxHeading=<h1 id="heading">react is basic necessity!!</h1>;
+console.log(jsxHeading);
 
 const root=ReactDOM.createRoot(document.getElementById("root"));
-
-root.render(heading);
+root.render(jsxHeading);
