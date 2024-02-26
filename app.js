a840b6e64ac70d30076b460d4772d587f0309afd
Sahil-Sukhdeve12 <sahilsukhdeve15@gmail.com>
2024-02-26 19:13:18 +0530

    jsx 26 feb

--- app.js
+++ app.js
@@ -3,8 +3,18 @@ import ReactDOM from "react-dom/client";
 
 //jsx
 
-const jsxHeading=<h1 id="heading">react is basic necessity!!</h1>;
-console.log(jsxHeading);
+const jsxHeading=<h1 id="heading" className="root">react is basic necessity!!</h1>;
+
+const jsxHeading1=(<h1 
+    id="heading" 
+    className="root">
+        react is basic necessity!!
+        </h1>); 
+        // writting in multiple lines 
+
+// console.log(jsxHeading);
+
+
 
 const root=ReactDOM.createRoot(document.getElementById("root"));
 root.render(jsxHeading);
