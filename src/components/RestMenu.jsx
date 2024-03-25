60594c17ee6fc1de011d91d757ef9541c84b2ca9
Sahil-Sukhdeve12 <sahilsukhdeve15@gmail.com>
2024-03-25 18:43:00 +0530

    25th march

--- src/components/RestMenu.jsx
+++ src/components/RestMenu.jsx
@@ -16,25 +16,25 @@ export default function RestMenu(){
         const data=await fetch(MENU_API+resId);
         const json=await data.json();
 
-        console.log(json);
+        //console.log(json);
         setresInfo(json.data);
     };
 
     if (resInfo===null ) return <Shimmer/>;
 
-    const rest = resInfo?.data?.cards[0]?.card?.card?.info;
-    const itemCard = resInfo?.data?.cards[2]?.groupedCard?.cardGroupMap?.REGULAR?.cards[2]?.card?.card?.itemCards;
-    console.log(itemCard);
+    const {name,cuisines,costForTwoMessage} = resInfo?.cards[0]?.card?.card?.info;
+    const {itemCards} = resInfo?.cards[2]?.groupedCard?.cardGroupMap?.REGULAR?.cards[1]?.card?.card;
+    //  console.log(itemCards);
 
     return (
         <div className="menu">
-            <h1>{rest.name}</h1>
-            <h3>{rest.costForTwo}</h3>
+            <h1>{name}</h1> 
+            {/* <h3>{costForTwoMessage/100}</h3> */}
             <p>
-                Cuisines: {rest?.cuisines.join(",")}
+                Cuisines: {cuisines.join(",")}
             </p>
             <ul>
-                {itemCard?.map((item)=>(
+                {itemCards?.map((item)=>(
                     <li key={item.card.info.id}>
                         <h3>{item.card.info.name}</h3>
                         <p>{item.card.info.description}</p>
